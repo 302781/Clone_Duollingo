5f249f856186a929283f636ea24a23be4b07d912
MaVi <131026303+302781@users.noreply.github.com>
2025-08-09 15:25:02 -0300

    Nova Pasta

---
+++ Linguas/app/buttons/page.tsx
@@ -0,0 +1,6 @@
+//Criar nova pagina para os botoes
+const ButtonsPage = () =>{
+    return(
+        <div>Botao da Pagina!</div>
+    );
+}
--- Linguas/app/layout.tsx
+++ Linguas/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";//Muda as fontes
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = Nunito({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const font = Nunito({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${font.variable} ${font.variable} antialiased`}
       >
         {children}
       </body>
