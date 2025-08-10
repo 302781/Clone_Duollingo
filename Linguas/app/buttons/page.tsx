38d8f7856e21b31cc6098b4d8100577e7980a2ab
MaVi <131026303+302781@users.noreply.github.com>
2025-08-10 17:53:02 -0300

    botoes

--- Linguas/app/buttons/page.tsx
+++ Linguas/app/buttons/page.tsx
@@ -1,9 +1,24 @@
 //Criar nova pagina para os botoes
+
+//Botao "Padrao" fica em branco
+//Botao "Primario" fica em azul
+//Botao "Esboço Primario" letra em azul fundo branco
+import {Button} from "@/components/ui/button";
 const ButtonsPage = () =>{
     return(
         <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
-            <button>Primary</button>
-            <button>Primary Ouline</button>
+            <Button>Padrao</Button> 
+            <Button variant="primary">Primario(a)</Button>
+            <Button variant="primaryOutline">Esboço Primário</Button>
+            <Button variant="segundary">Segundario(a)</Button>
+            <Button variant="segundaryOutline">Esboço SEgundario</Button>
+            <Button variant="danger">Perigo</Button>
+            <Button variant="dangerOutline">Esboço Perigo</Button>
+            <Button variant="Super">Super</Button>
+            <Button variant="SuperOutline">Esboço Super</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="sidebar">Sidebar</Button>
+            <Button variant="sidebarrOutline">Esboço Sidebar</Button>
         </div>
     );
 };
--- Linguas/components/ui/button.tsx
+++ Linguas/components/ui/button.tsx
@@ -5,12 +5,22 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive disabled:opacity-50 uppercase tracking-wide",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm font-bold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 uppercase tracking-wide",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500",
+          primary:"bg-sky-408 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
+          primaryOutline: "bg-white text-sky-500 hover:bg-slate-10",
+          segundary:"bg-green-500 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",//trocar cor do botao no bg-"green" e intensidade da cor "500"
+          segundaryOutline: "bg-white text-green-500 hover:bg-slate-10",//trocar cor do texto "text-green"
+          danger:"bg-rose-408 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
+          dangerOutline: "bg-white text-rose-500 hover:bg-slate-10",
+          Super:"bg-indigo-408 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
+          SuperOutline: "bg-white text-indigo-500 hover:bg-slate-10",
+          ghost:"bg-indigo-408 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
+          sidebar:"bg-indigo-408 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
         destructive:
           "bg-green-500 text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -23,10 +33,11 @@ const buttonVariants = cva(
         //premium: "bg-indigo-500 text-white"//tirar a troca de cor do botao la no variant da pagina "page"
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
+        default: "h-10 px-4 py-2 ",
+        sm: "h-9 px-3",
+        lg: "h-12 px-8",
+        icon: "h-10 w-10",
+        "full-rounded": "h-10 w-10 rounded-full",
       },
     },
     defaultVariants: {
