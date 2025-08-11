2a46b3d683af6336c9be2a07f3644a1c6888a7ff
MaVi <131026303+302781@users.noreply.github.com>
2025-08-11 13:40:29 -0300

    Pagina Marketing

---
+++ Linguas/app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  Children: React.ReactNode;
+};
+
+const MarketingLayount = ({ children }: Props) => {
+    return(
+        <div className="min-h-screen flex flex-col">
+            <main className="flex-1 flex flex-col items-center justify-center">
+            </main>
+        </div>
+    );
+};
+
+export default MarketingLayount;
