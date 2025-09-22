9203f739f7ee0a579a36d2f4f01a9a53fc8c8ef2
MaVi <131026303+302781@users.noreply.github.com>
2025-09-22 10:24:20 -0300

    componentes

---
+++ Linguas/app/(marketing)/header.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return(
+        <footer className="hidden lg:block h-20 w-full boeder-t-2 border-slaye-200 p-2">
+            Rodape
+        </footer>
+    );
+}
--- Linguas/app/(marketing)/layout.tsx
+++ Linguas/app/(marketing)/layout.tsx
@@ -5,10 +5,13 @@ type Props = {
 const MarketingLayount = ({ children }: Props) => {
     return(
         <div className="min-h-screen flex flex-col">
+            <header/>
             <main className="flex-1 flex flex-col items-center justify-center">
+                {children}
             </main>
+            <footer/>
         </div>
-    );
+    ); 
 };
 
 export default MarketingLayount;
